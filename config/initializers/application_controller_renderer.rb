7eb6e62f2309691d065796795274b15628354cb5
EC2 Default User <ec2-user@ip-172-31-11-135.ap-northeast-1.compute.internal>
2020-11-28 07:21:17 +0000

    seakret.key追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -4,3 +4,4 @@
 #   http_host: 'example.org',
 #   https: false
 # )
+Refile.secret_key = '0185cea9e619a2af42cd2909480d2d1925af5a7955019809fbbb8c1f7d6d3f4cdddf455366234e10c1e43fd6d2657f7bd8713eb94cb7a2729e28144f490acbff'
